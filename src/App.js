be73f3b2d1203c3eac8267a59aa696895d9be738
Rohit <rohit43212@gmail.com>
2020-05-25 10:52:02 -0700

    Added base component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Projects from "Components/Projects";
+import Experience from "Components/Experience";
+import About from "Components/About";
+import Skills from "Components/Skills";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h2>portfolio</h2>
+    <div className="App">
+      <About />
+      <Experience />
+      <Skills />
+      <Projects />
     </div>
   );
 }
